6a39e55404c096a25247943137c15eda537fa9dc
Brendan Berg <brendan_berg@aol.com>
2017-08-19 20:21:24 +0200

    imporoved timeController

--- timeController.py
+++ timeController.py
@@ -9,25 +9,24 @@ class TimeController( object ):
     def __init__( self, interval ):
         self.eventInterval = interval
         self.lastTimedEvent = time.time( )
-        self.speed = 1
+        self.speedMode = 3
+        self.speedSet = [ 0.125, 0.25, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 
 
     def getIntvProgress( self ):
-        return ( time.time( ) - self.lastTimedEvent ) / self.eventInterval
+        return ( time.time( ) - self.lastTimedEvent ) / ( self.eventInterval / self.speedSet[ self.speedMode ] )
 
     def incSpeed( self ):
-        self.eventInterval /= 2
-        self.speed *= 2
+        self.speedMode = min( self.speedMode+1, 14 )
 
     def decSpeed( self ):
-        self.eventInterval *= 2
-        self.speed /= 2
+        self.speedMode = max( self.speedMode-1, 0 )
 
     def getSpeed( self ):
-        return self.speed
+        return self.speedSet[ self.speedMode ]
 
     def timeEvent( self ):
-        if time.time( ) > self.lastTimedEvent + self.eventInterval:
+        if time.time( ) > self.lastTimedEvent + ( self.eventInterval / self.speedSet[ self.speedMode ] ):
             self.lastTimedEvent = time.time( )
             return True
         return False
